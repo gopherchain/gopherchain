f82c09eb1a64d4175b75ba06a82fcd86818c348d
Juan Huttemann <juanfhuttemann@gmail.com>
2020-09-08 11:02:07 -0400

    :building_construction: move block functions to block module

--- main.go
+++ main.go
@@ -1,46 +1,20 @@
 package main
 
 import (
-	"bytes"
-	"crypto/sha256"
+	"fmt"
+
+	"github.com/juanhuttemann/gochain/blockchain"
 )
 
-type Block struct {
-	Hash     []byte
-	Data     []byte
-	PrevHash []byte
-}
-
-type BlockChain struct {
-	blocks []*Block
-}
-
-func (b *Block) DeriveHash() {
-	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
-	hash := sha256.Sum256(info)
-	b.Hash = hash[:]
-}
-
-func CreateBlock(data string, prevHash []byte) *Block {
-	block := &Block{[]byte{}, []byte(data), prevHash}
-	block.DeriveHash()
-	return block
-}
-
-func (chain *BlockChain) AddBlock(data string) {
-	prevBlock := chain.blocks[len(chain.blocks)-1]
-	new := CreateBlock(data, prevBlock.Hash)
-	chain.blocks = append(chain.blocks, new)
-}
-
-func Genesis() *Block {
-	return CreateBlock("Genesis", []byte{})
-}
-
-func InitBlockChain() *BlockChain {
-	return &BlockChain{[]*Block{Genesis()}}
-}
-
 func main() {
+	chain := blockchain.InitBlockChain()
+	chain.AddBlock("First Block After Genesis")
+	chain.AddBlock("Second Block After Genesis")
+	chain.AddBlock("Third Block After Genesis")
 
+	for _, block := range chain.Blocks {
+		fmt.Println("Prev Hash:", block.PrevHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Println("Hash", block.Hash)
+	}
 }
