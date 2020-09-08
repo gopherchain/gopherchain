f1dcdd1f1057957ecebe42ba96cf7714b734ddd5
Juan Huttemann <juanfhuttemann@gmail.com>
2020-09-08 10:07:15 -0400

    :triangular_flag_on_post: add CreateBlock, AddBlock and Genesis functions

--- main.go
+++ main.go
@@ -11,10 +11,30 @@ type Block struct {
 	PrevHash []byte
 }
 
+type BlockChain struct {
+	blocks []*Block
+}
+
 func (b *Block) DeriveHash() {
 	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
 	hash := sha256.Sum256(info)
 	b.Hash = hash[:]
+}
+
+func CreateBlock(data string, prevHash []byte) *Block {
+	block := &Block{[]byte{}, []byte(data), prevHash}
+	block.DeriveHash()
+	return block
+}
+
+func (chain *BlockChain) AddBlock(data string) {
+	prevBlock := chain.blocks[len(chain.blocks)-1]
+	new := CreateBlock(data, prevBlock.Hash)
+	chain.blocks = append(chain.blocks, new)
+}
+
+func Genesis() *Block {
+	return CreateBlock("Genesis", []byte{})
 }
 
 func main() {
