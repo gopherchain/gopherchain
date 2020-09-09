0e70058e4fc60b8a673a302d374c582428ce2667
Juan Huttemann <juanfhuttemann@gmail.com>
2020-09-09 15:05:50 -0400

    :triangular_flag_on_post: add Serialize and Deserialize func

--- blockchain/block.go
+++ blockchain/block.go
@@ -1,5 +1,11 @@
 package blockchain
 
+import (
+	"bytes"
+	"encoding/gob"
+	"log"
+)
+
 type Block struct {
 	Hash     []byte
 	Data     []byte
@@ -20,4 +26,29 @@ func CreateBlock(data string, prevHash []byte) *Block {
 
 func Genesis() *Block {
 	return CreateBlock("Genesis", []byte{})
+}
+
+func (b *Block) Serialize() []byte {
+	var res bytes.Buffer
+	enconder := gob.NewEncoder(&res)
+
+	err := enconder.Encode(b)
+	if err != nil {
+		log.Panic(err)
+	}
+	return res.Bytes()
+}
+
+func Deserialize(data []byte) *Block {
+	var block Block
+
+	decoder := gob.NewDecoder(bytes.NewReader(data))
+
+	err := decoder.Decode(&block)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return &block
 }
