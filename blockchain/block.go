e26bfb79da12bd2d91c75b9dc3f0ae6e2acd3abd
Juan Huttemann <juanfhuttemann@gmail.com>
2020-09-09 14:50:55 -0400

    :recycle: moving blockchain specifics to its own file

--- blockchain/block.go
+++ blockchain/block.go
@@ -7,10 +7,6 @@ type Block struct {
 	Nonce    int
 }
 
-type BlockChain struct {
-	Blocks []*Block
-}
-
 func CreateBlock(data string, prevHash []byte) *Block {
 	block := &Block{[]byte{}, []byte(data), prevHash, 0}
 	pow := NewProof(block)
@@ -22,16 +18,6 @@ func CreateBlock(data string, prevHash []byte) *Block {
 	return block
 }
 
-func (chain *BlockChain) AddBlock(data string) {
-	prevBlock := chain.Blocks[len(chain.Blocks)-1]
-	new := CreateBlock(data, prevBlock.Hash)
-	chain.Blocks = append(chain.Blocks, new)
-}
-
 func Genesis() *Block {
 	return CreateBlock("Genesis", []byte{})
-}
-
-func InitBlockChain() *BlockChain {
-	return &BlockChain{[]*Block{Genesis()}}
 }
