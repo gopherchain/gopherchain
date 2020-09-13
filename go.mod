3595a5f5e9f7cc70d128277c5dac1046e03b4c38
Juan Huttemann <juanfhuttemann@gmail.com>
2020-09-13 16:16:29 -0400

    :sparkles: add wallets

--- go.mod
+++ go.mod
@@ -5,4 +5,6 @@ go 1.13
 require (
 	github.com/dgraph-io/badger/v2 v2.2007.2
 	github.com/kr/pretty v0.2.0 // indirect
+	github.com/mr-tron/base58 v1.2.0
+	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a
 )
---
+++ wallet/wallets.go
@@ -0,0 +1,107 @@
+package wallet
+
+import (
+	"bytes"
+	"crypto/elliptic"
+	"encoding/gob"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+const (
+	walletDir        = "./mywallets"
+	walletFile       = "wallets.data"
+	pathToWalletFile = walletDir + "/" + walletFile
+)
+
+type Wallets struct {
+	Wallets map[string]*Wallet
+}
+
+func (ws *Wallets) GetAllAddresses() []string {
+	var addresses []string
+
+	for address := range ws.Wallets {
+		addresses = append(addresses, address)
+	}
+
+	return addresses
+}
+
+func CreateWallets() (*Wallets, error) {
+	wallets := Wallets{}
+	wallets.Wallets = make(map[string]*Wallet)
+
+	err := wallets.LoadFile()
+	return &wallets, err
+}
+
+func (ws *Wallets) AddWallet() string {
+	wallet := MakeWallet()
+	address := fmt.Sprintf("%s", wallet.Address())
+
+	ws.Wallets[address] = wallet
+
+	return address
+}
+
+func (ws Wallets) GetWallet(address string) Wallet {
+	return *ws.Wallets[address]
+}
+
+func (ws *Wallets) LoadFile() error {
+	_, err := os.Stat(pathToWalletFile)
+
+	if os.IsNotExist(err) {
+		err := os.MkdirAll(walletDir, os.ModePerm)
+		if err != nil {
+			return err
+		}
+		walletsFile, err := os.OpenFile(pathToWalletFile, os.O_RDONLY|os.O_CREATE, 0666)
+
+		if err != nil {
+			return err
+		}
+		defer walletsFile.Close()
+	}
+
+	if _, err := os.Stat(pathToWalletFile); os.IsNotExist(err) {
+		return err
+	}
+
+	var wallets Wallets
+
+	fileContent, err := ioutil.ReadFile(pathToWalletFile)
+	if err != nil {
+		return err
+	}
+
+	gob.Register(elliptic.P256())
+	decoder := gob.NewDecoder(bytes.NewReader(fileContent))
+	err = decoder.Decode(&wallets)
+	if err != nil {
+		return err
+	}
+
+	ws.Wallets = wallets.Wallets
+
+	return nil
+}
+
+func (ws *Wallets) SaveFile() {
+	var content bytes.Buffer
+	gob.Register(elliptic.P256())
+
+	encoder := gob.NewEncoder(&content)
+	err := encoder.Encode(ws)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	err = ioutil.WriteFile(pathToWalletFile, content.Bytes(), 0644)
+	if err != nil {
+		log.Panic(err)
+	}
+}
