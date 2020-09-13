ca6738ac261902c9b22b6f0706644c53d9c9e416
Juan Huttemann <juanfhuttemann@gmail.com>
2020-09-12 23:00:41 -0400

    move tx strucs and functions to its own file

--- blockchain/transaction.go
+++ blockchain/transaction.go
@@ -9,17 +9,6 @@ import (
 	"log"
 )
 
-type TxInput struct {
-	ID  []byte
-	Out int
-	Sig string
-}
-
-type TxOutput struct {
-	Value  int
-	PubKey string
-}
-
 type Transaction struct {
 	ID      []byte
 	Inputs  []TxInput
@@ -86,12 +75,4 @@ func NewTransaction(from, to string, amount int, chain *BlockChain) *Transaction
 
 func (tx *Transaction) IsCoinbase() bool {
 	return len(tx.Inputs) == 1 && len(tx.Inputs[0].ID) == 0 && tx.Inputs[0].Out == -1
-}
-
-func (in *TxInput) CanUnlock(data string) bool {
-	return in.Sig == data
-}
-
-func (out *TxOutput) CanBeUnlocked(data string) bool {
-	return out.PubKey == data
 }
