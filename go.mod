895b4e8d8ff4bcc4a3ace1dd3fc08652b8038ff3
Juan Huttemann <juanfhuttemann@gmail.com>
2020-09-17 01:59:12 -0400

    :construction: initial network module

--- go.mod
+++ go.mod
@@ -7,4 +7,5 @@ require (
 	github.com/dgraph-io/badger/v2 v2.2007.2
 	github.com/mr-tron/base58 v1.2.0
 	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a
+	gopkg.in/vrecan/death.v3 v3.0.1
 )
---
+++ network/network.go
@@ -0,0 +1,98 @@
+package network
+
+import (
+	"fmt"
+	"os"
+	"runtime"
+	"syscall"
+
+	"gopkg.in/vrecan/death.v3"
+
+	"github.com/juanhuttemann/gochain/blockchain"
+)
+
+const (
+	protocol      = "tcp"
+	version       = 1
+	commandLength = 12
+)
+
+var (
+	nodeAddress     string
+	minerAddress    string
+	knownNodes      = []string{"localhost:3000"}
+	blocksInTransit = [][]byte{}
+	memoryPool      = make(map[string]blockchain.Transaction)
+)
+
+type Addr struct {
+	AddrList []string
+}
+
+type Block struct {
+	AddFrom string
+	Block   []byte
+}
+
+type GetBlocks struct {
+	AddrFrom string
+}
+
+type GetData struct {
+	AddrFrom string
+	Type     string
+	ID       []byte
+}
+
+type Inv struct {
+	AddrFrom string
+	Type     string
+	Items    [][]byte
+}
+
+type Tx struct {
+	AddrFrom    string
+	Transaction []byte
+}
+
+type Version struct {
+	Version    int
+	BestHeight int
+	AddrFrom   string
+}
+
+func CmdToBytes(cmd string) []byte {
+	var bytes [commandLength]byte
+
+	for i, c := range cmd {
+		bytes[i] = byte(c)
+	}
+
+	return bytes[:]
+}
+
+func BytesToCmd(bytes []byte) string {
+	var cmd []byte
+
+	for _, b := range bytes {
+		if b != 0x0 {
+			cmd = append(cmd, b)
+		}
+	}
+
+	return fmt.Sprintf("%s", cmd)
+}
+
+func ExtractCmd(request []byte) []byte {
+	return request[:commandLength]
+}
+
+func CloseDB(chan *blockchain.BlockChain) {
+	d := death.NewDeath(syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+
+	d.WaitForDeathWithFunc(func() {
+		defer os.Exit(1)
+		defer runtime.Goexit()
+		chain.Database.Close()
+	})
+}
