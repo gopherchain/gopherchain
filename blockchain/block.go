1ef5422b84badc86350bfc493897932599b56041
Juan Huttemann <juanfhuttemann@gmail.com>
2020-09-16 21:58:53 -0400

    :sparkles: add merkle tree

--- blockchain/block.go
+++ blockchain/block.go
@@ -2,7 +2,6 @@ package blockchain
 
 import (
 	"bytes"
-	"crypto/sha256"
 	"encoding/gob"
 )
 
@@ -15,13 +14,14 @@ type Block struct {
 
 func (b *Block) HashTransactions() []byte {
 	var txHashes [][]byte
-	var txHash [32]byte
-	for _, tx := range b.Transactions {
-		txHashes = append(txHashes, tx.ID)
-	}
-	txHash = sha256.Sum256(bytes.Join(txHashes, []byte{}))
 
-	return txHash[:]
+	for _, tx := range b.Transactions {
+		txHashes = append(txHashes, tx.Serialize())
+	}
+
+	tree := NewMerkleTree(txHashes)
+
+	return tree.RootNode.Data
 }
 
 func CreateBlock(txs []*Transaction, prevHash []byte) *Block {
