7add77cfd25ff6c20901483df71b562c2f288d14
Juan Huttemann <juanfhuttemann@gmail.com>
2020-09-25 17:25:35 -0400

    :sparkles: moving known nodes to a file

---
+++ network/utils.go
@@ -0,0 +1,43 @@
+package network
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func fileExists(filename string) bool {
+	if _, err := os.Stat(filename); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+	return true
+}
+
+func fileEmpty(filename string) bool {
+	file, err := os.Stat(filename)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	if file.Size() == 0 {
+		return true
+	}
+
+	return false
+}
+
+func defaultNodeConfig(filename string) {
+	fmt.Println("Creating default nodes file")
+	f, err := os.OpenFile(filename,
+		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Println(err)
+	}
+	defer f.Close()
+
+	if _, err := f.WriteString("localhost:777\n"); err != nil {
+		log.Println(err)
+	}
+}
