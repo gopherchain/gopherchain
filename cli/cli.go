d960d12e2ace7ec3c95ea9226cfccea4e4724e78
Juan Huttemann <juanfhuttemann@gmail.com>
2020-09-25 18:35:18 -0400

    move file server to goroutine

--- cli/cli.go
+++ cli/cli.go
@@ -38,9 +38,11 @@ func (cli *CommandLine) validateArgs() {
 func (cli *CommandLine) StartNode(nodeID, minerAddress string) {
 	fmt.Printf("Starting Node %s\n", nodeID)
 
-	fs := http.FileServer(http.Dir("./blocks"))
+	go func() {
+		fs := http.FileServer(http.Dir("./blocks"))
 
-	log.Fatal(http.ListenAndServe(":3002", fs))
+		log.Fatal(http.ListenAndServe(":3002", fs))
+	}()
 
 	if len(minerAddress) > 0 {
 		if wallet.ValidateAddress(minerAddress) {
